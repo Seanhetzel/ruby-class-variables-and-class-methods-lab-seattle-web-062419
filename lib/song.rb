ae54517276b71c838f89fa97d4ae207a8b58dcac
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-01 03:21:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,20 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << artist
   end
 
   def self.genre_count
     @@count
   end
+
+  def self.artist_count
+    @@artists
 end
