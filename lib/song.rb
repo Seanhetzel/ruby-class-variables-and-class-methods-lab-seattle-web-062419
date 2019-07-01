dd8c7c39e2cd6c5554f4ff1a44eb9cb0976f4146
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-01 03:49:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   def self.artist_count
     artists_hash = {}
     @@artists.each do |artist|
-      if artists_hash.has_key(artist)
+      if artists_hash.has_key?(artist)
         artists_hash[artist] += 1
       else
         artists_hash[artist] = 1
